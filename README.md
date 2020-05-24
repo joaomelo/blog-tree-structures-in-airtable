00f12fb5be3e677c6ad5706b74ef207bd1043133
joao <j@vmelo.net>
2020-05-24 14:29:42 -0300

    initial setup

---
+++ README.md
@@ -0,0 +1,2 @@
+The repository contains support code for the post How to Create a Dialog to Select Multiple Values in Google Sheets
+
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+'use strict';
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+const dist = path.resolve(__dirname, './dist');
+const src = path.resolve(__dirname, './src');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    publicPath: '/',
+    path: dist,
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    hot: true,
+    port: 8080,
+    historyApiFallback: true,
+    noInfo: false,
+    stats: 'normal',
+    watchOptions: {
+      poll: true
+    }
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('tailwindcss'),
+              ],
+            },            
+          }
+        ],
+      },      
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({ template: src + '/index.html' }),
+  ]
+};
