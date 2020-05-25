a500533a59f0f99b89131ab14fa4726a04b277cb
joao <j@vmelo.net>
2020-05-24 21:10:58 -0300

    read ui signal number of records read

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
+import Airtable from 'airtable';
 import './index.css'
 
 const run = document.getElementById('run');
 run.onclick = () => {
-  const logs = document.getElementById('logs');
-  logs.innerHTML = 'button clicked';
+  const apiKey = document.getElementById('key').value;
+  const baseId = document.getElementById('base').value;
+  const tableName = document.getElementById('table').value;
+
+  const table = createTable({ apiKey, baseId, tableName });
+  const loadPromise = loadRecords(table);
+
+  loadPromise.then(records => {
+    const logs = document.getElementById('logs');
+    logs.innerHTML = `loaded ${records.length} record(s) from ${tableName}`;
+  })
+}
+
+function createTable({apiKey, baseId, tableName}) {
+  return new Airtable({ apiKey })
+    .base(baseId)
+    .table(tableName)
+}
+
+function loadRecords(table) {
+  const allRecords = [];
+  return table
+    .select()
+    .eachPage((pageRecords, fetchNextPage) => {
+      pageRecords.forEach(r => allRecords.push(r));
+      fetchNextPage();
+    })
+    .then(() => allRecords);
 }
